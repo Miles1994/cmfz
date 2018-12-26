0685c5abbbcdf0d145d940edc85f49f9479246af
Miles <872564905@qq.com>
2018-12-26 21:30:51 +0800

    1.6.1 SpringBoot集成FastDfs 和 Nginx集成

--- src/test/java/com/cmfz/ssmcmfzApplicationTests.java
+++ src/test/java/com/cmfz/ssmcmfzApplicationTests.java
@@ -1,43 +1,31 @@
 package com.cmfz;
 
-import cn.afterturn.easypoi.excel.ExcelExportUtil;
-import cn.afterturn.easypoi.excel.entity.ExportParams;
-import com.cmfz.entity.Album;
-import com.cmfz.mapper.AlbumMapper;
-import org.apache.poi.ss.usermodel.Workbook;
+
+import com.github.tobato.fastdfs.domain.fdfs.StorePath;
+import com.github.tobato.fastdfs.service.FastFileStorageClient;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.List;
+import java.io.*;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ssmcmfzApplicationTests {
     @Resource
-    private AlbumMapper albumMapper;
+    private FastFileStorageClient storageClient;
 
     @Test
-    public void testPoi() {
-        List<Album> albums = albumMapper.queryAlbum();
-
-        for (Album album : albums) {
-            String cover_img = album.getCover_img();
-            String npath = "E:\\MyIdeaProject\\ssmcmfz\\src\\main\\webapp\\image\\" + cover_img;
-            album.setCover_img(npath);
-        }
-        Workbook workbook = ExcelExportUtil.exportExcel(new ExportParams("专辑详情", "专辑"),
-                Album.class, albums);
-        try {
-            workbook.write(new FileOutputStream(new File("E:/aaaaaa.xls")));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public void testPoi() throws FileNotFoundException {
+        File file = new File("E:\\aa.jpg");
+        FileInputStream inputStream = new FileInputStream(file);
+        StorePath storePath = storageClient.uploadFile(inputStream,
+                file.length(), "jpg", null);
+        System.out.println(storePath.getGroup() + "|" + storePath.getPath());
     }
 }
 
